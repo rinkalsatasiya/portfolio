05b6121cc123365b64649466249392b7f2d0bcf7
rinkalsatasiya <rinkalsatasiya>
2020-02-17 18:10:13 +0530

    remove extra menu

--- index.php
+++ index.php
@@ -41,26 +41,12 @@
         <div class="row align-items-center">
           <div class="site-logo col-6"><a href="index.html">Scenic</a></div>
 
-
-          
-
           <nav class="mx-auto site-navigation">
             <ul class="site-menu js-clone-nav d-none d-lg-block">
               <li><a href="index.html" class="nav-link active">Home</a></li>
               <li><a href="about.html">About</a></li>
               <li><a href="portfolio.html">Portfolio</a></li>
-              <li class="has-children">
-                <a href="services.html">Pages</a>
-                <ul class="dropdown">
-                  <li><a href="services.html">Services</a></li>
-                  <li><a href="service-single.html">Service Single</a></li>
-                  <li><a href="blog-single.html">Blog Single</a></li>
-                  <li><a href="portfolio-single.html">Portfolio Single</a></li>
-                  <li><a href="testimonials.html">Testimonials</a></li>
-                  <li><a href="faq.html">Frequently Ask Questions</a></li>
-                  <li><a href="gallery.html">Gallery</a></li>
-                </ul>
-              </li>
+              
               <li><a href="blog.html">Blog</a></li>
               <li><a href="contact.html">Contact</a></li>
             </ul>
